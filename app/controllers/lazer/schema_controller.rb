e123fb3cac75243d2353d96633cc0bd9b3ad899c
Vic Ramon <v@vicramon.com>
2020-11-29 14:14:30 -0600

    Cleanup code

--- app/controllers/lazer/schema_controller.rb
+++ app/controllers/lazer/schema_controller.rb
@@ -3,39 +3,68 @@ require_dependency "lazer/application_controller"
 module Lazer
   class SchemaController < ApplicationController
 
+    # todo: page this endpoint so it won't hit as hard
     def show
       authenticate!; return if performed?
 
       Rails.application.eager_load!
       models = ActiveRecord::Base.descendants
 
-      result = models.map do |model|
-        model_data = {
-          name: model.name,
-          table: model.table_name,
-        }
-        associations = []
-        begin
-        model.reflect_on_all_associations.each do |association|
-          associations << {
-            marcro: association.macro,
-            name: association.name,
-            options: association.options,
-            table: association.table_name,
-          }
-        end
-        rescue => e
-          logger.debug "error #{e.message}"
-        end
-        model_data[:scopes] = model.instance_variable_get(:@__scopes__)
-        model_data[:associations] = associations
-        model_data
+      result = {}
+
+      result[:models] = models.map do |model|
+        parse_model(model)
       end
 
-      render json: result
+      # not necessarily needed, can get tables directly from the db
+      # result[:raw_tables] = gather_tables
+
+      render json: result.to_json
     end
 
     private
+
+    def connection
+      ActiveRecord::Base.connection
+    end
+
+    def gather_tables
+      connection.tables.map do |table_name|
+        { table_name =>
+          connection.columns(table_name).map do |column|
+            {
+              name: column.name,
+              type: column.type,
+            }
+          end
+        }
+      end
+    end
+
+    def parse_model(model)
+      logger.debug "Parsing: #{model.name}"
+      # return {} if model.name == "ActiveRecord::SchemaMigration"
+      model_data = {
+        name: model.name,
+        table: model.table_name,
+      }
+      associations = []
+      model.reflect_on_all_associations.each do |association|
+        associations << {
+          marcro: association.macro,
+          name: association.name,
+          options: association.options,
+          table: association.table_name,
+        }
+      end
+      # model_data[:columns] = model.column_names
+      # model_data[:scopes] = model.instance_variable_get(:@__scopes__)
+      model_data[:associations] = associations
+      return model_data
+    rescue => e
+      logger.debug "error parsing model #{model.name}: #{e.message}"
+      return {}
+    end
 
     def authenticate!
       if params[:lazer_key].blank?
